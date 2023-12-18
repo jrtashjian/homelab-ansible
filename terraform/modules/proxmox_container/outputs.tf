9f55cc5bbdcca867dabf85544f3992467f63404d
JR Tashjian <hello@jrtashjian.com>
2023-12-18 02:23:10 -0500

    Update outputs to provide more information about the created LXCs

--- terraform/modules/proxmox_container/outputs.tf
+++ terraform/modules/proxmox_container/outputs.tf
@@ -1,9 +1,19 @@
 output "name" {
-  description = "List of LXC names created by the module."
+  description = "List of names of the LXC created by the module."
   value       = join(",", proxmox_virtual_environment_container.base_lxc[*].initialization[0].hostname)
 }
 
 output "ipv4_address" {
-  description = "List of VM names created by the module."
-  value       = proxmox_virtual_environment_container.base_lxc[*].initialization[0].ip_config[0].ipv4[0].address
+  description = "List of IPv4 addresses of the LXC created by the module."
+  value       = tolist(proxmox_virtual_environment_container.base_lxc[*].initialization[0].ip_config[0].ipv4[0].address)[0]
+}
+
+output "id" {
+  description = "List of IDs of the LXC created by the module."
+  value       = tolist(proxmox_virtual_environment_container.base_lxc[*].id)[0]
+}
+
+output "node_name" {
+  description = "List of node names of the LXC created by the module."
+  value       = tolist(proxmox_virtual_environment_container.base_lxc[*].node_name)[0]
 }

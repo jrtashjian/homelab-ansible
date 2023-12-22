6f86eefb631e6779cfb857fb53af82288f136c58
JR Tashjian <hello@jrtashjian.com>
2023-12-22 17:07:53 -0500

    Import and manage vmbr0

--- terraform/stacks/pve-cluster/main.tf
+++ terraform/stacks/pve-cluster/main.tf
@@ -24,8 +24,14 @@ provider "proxmox" {
 
 locals {
   pve_nodes = {
-    "pve-node02" = "192.168.30.12/24"
-    "pve-node03" = "192.168.30.13/24"
+    "pve-node02" = {
+      "vmbr0" = "192.168.10.12/24"
+      "vmbr1" = "192.168.30.12/24"
+    }
+    "pve-node03" = {
+      "vmbr0" = "192.168.10.13/24"
+      "vmbr1" = "192.168.30.13/24"
+    }
   }
 }
 
@@ -38,6 +44,27 @@ resource "proxmox_virtual_environment_certificate" "int_jrtashjian_com" {
   private_key = trimspace(var.int_jrtashjian_com_key)
 }
 
+# vmbr0 is created during the installation of Proxmox VE.
+import {
+  to = proxmox_virtual_environment_network_linux_bridge.vmbr0["pve-node02"]
+  id = "pve-node02:vmbr0"
+}
+import {
+  to = proxmox_virtual_environment_network_linux_bridge.vmbr0["pve-node03"]
+  id = "pve-node03:vmbr0"
+}
+resource "proxmox_virtual_environment_network_linux_bridge" "vmbr0" {
+  for_each  = local.pve_nodes
+  node_name = each.key
+
+  name    = "vmbr0"
+  comment = "Managed by Terraform"
+
+  ports   = ["eno2"]
+  address = each.value["vmbr0"]
+  gateway = "192.168.10.1"
+}
+
 # Create a Linux bridge for the storage network.
 resource "proxmox_virtual_environment_network_linux_bridge" "vmbr1" {
   for_each  = local.pve_nodes
@@ -47,7 +74,7 @@ resource "proxmox_virtual_environment_network_linux_bridge" "vmbr1" {
   comment = "Managed by Terraform"
 
   ports   = ["eno3"]
-  address = each.value
+  address = each.value["vmbr1"]
 }
 
 # Add firewall aliases.

f42249f152fb511cdcb83440e87e73792cba0c21
JR Tashjian <hello@jrtashjian.com>
2024-02-05 01:43:03 -0500

    Remove debian_template

--- terraform/stacks/pve-cluster/variables.tf
+++ terraform/stacks/pve-cluster/variables.tf
@@ -1,25 +1,3 @@
-variable "node02_user" {
-  description = "pve-node02 user"
-  type        = string
-}
-
-variable "node02_pass" {
-  description = "pve-node02 password"
-  type        = string
-  sensitive   = true
-}
-
-variable "node03_user" {
-  description = "pve-node03 user"
-  type        = string
-}
-
-variable "node03_pass" {
-  description = "pve-node03 password"
-  type        = string
-  sensitive   = true
-}
-
 variable "ansible_user" {
   description = "Ansible user"
   type        = string

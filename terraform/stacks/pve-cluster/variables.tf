89001ad68d110655aa08f8d6b0ca0240269a169e
JR Tashjian <hello@jrtashjian.com>
2024-02-19 19:18:31 -0500

    Remove unnecessary cloud-config and variables

--- terraform/stacks/pve-cluster/variables.tf
+++ terraform/stacks/pve-cluster/variables.tf
@@ -13,15 +13,3 @@ variable "ansible_public_key" {
   description = "Ansible public key"
   type        = string
 }
-
-variable "int_jrtashjian_com_cert" {
-  description = "int.jrtashjian.com certificate"
-  type        = string
-  sensitive   = true
-}
-
-variable "int_jrtashjian_com_key" {
-  description = "int.jrtashjian.com private key"
-  type        = string
-  sensitive   = true
-}

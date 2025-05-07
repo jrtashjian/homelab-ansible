4d329b1eebea5596c1b67fdd6c5fd559932fa444
JR Tashjian <hello@jrtashjian.com>
2025-05-07 18:13:08 -0400

    cmangos: update proxmox and ansible provider versions to latest stable releases

--- terraform/stacks/game-cmangos/terraform.tf
+++ terraform/stacks/game-cmangos/terraform.tf
@@ -19,14 +19,14 @@ terraform {
   required_providers {
     proxmox = {
       source  = "bpg/proxmox"
-      version = "0.43.3"
+      version = "0.77.1"
     }
 
     ansible = {
       source  = "ansible/ansible"
-      version = "1.1.0"
+      version = "1.3.0"
     }
   }
 
-  required_version = ">= 1.6"
+  required_version = ">= 1.11"
 }
